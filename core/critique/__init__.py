8ec62eaf6a857c118c7b06147750731f8e705777
Dmitry Stepakov <dimanata1232@gmail.com>
2024-11-26 03:23:43 +0300

    emoji fix

--- core/critique/__init__.py
+++ core/critique/__init__.py
@@ -1,9 +1,7 @@
 from core.critique.common.processor import ССSProcessor
-from core.critique.emoji.proccesor import EmojiProcessor
 from core.critique.semantic.proccesor import SemanticSimProcessor
 
 __all__ = [
     "ССSProcessor",
-    "EmojiProcessor",
     "SemanticSimProcessor",
 ]
